1dc4829ef9f18a115a9dc2ca3879963efa5a006a
sginko <s.ginkota@gmail.com>
2023-08-16 19:32:41 +0200

    Some refactoring

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,39 +6,50 @@ import java.util.Scanner;
 //TODO 1. Application should not close after thrown exception.
 //TODO 2.
 
+
 public class Calculator {
 
     private int firstNumber;
     private int secondNumber;
     private int result;
-    private String exit = "no";
+    private String exit = "y";
     private final Scanner scan = new Scanner(System.in);
 
+
     public void run() {
+        System.out.println("-----------------------------");
+        System.out.println("Console calculator");
+        System.out.println("You can take nu");
+        System.out.println("-----------------------------");
 
         do {
-            logic();
-
-        } while (exit.equals("yes"));
+            number();
+            action();
+        } while (exit.equals("y"));
     }
 
-    private void logic() {
+    private void number() {
 
         try {
-
             System.out.print("Enter first number: ");
-            firstNumber = scan.nextInt();
 
-            System.out.print("Enter second number: ");
-            secondNumber = scan.nextInt();
-
+            if (scan.hasNextInt()) {
+                firstNumber = scan.nextInt();
+                System.out.print("Enter second number: ");
+                secondNumber = scan.nextInt();
+            } else {
+                System.out.println("It's not a number");
+//                System.out.print("Enter first number: ");
+            }
         } catch (InputMismatchException e) {
             System.err.println(e.getMessage());
-
         }
+    }
 
-            System.out.print("Enter action: ");
+
+    private void action() {
         try {
+            System.out.print("Enter action: ");
             switch (scan.next().charAt(0)) {
                 case '+' -> result = firstNumber + secondNumber;
                 case '-' -> result = firstNumber - secondNumber;
@@ -46,18 +57,15 @@ public class Calculator {
                 case '/' -> result = firstNumber / secondNumber;
                 default -> {
                     System.err.println("You input invalid action");
-
                 }
             }
             System.out.println("Result: " + result);
-            System.out.print("Do you want continue? no / yes: ");
-            exit = scan.next();
-
         } catch (ArithmeticException e) {
-            System.err.println("Division by zero");
-
+            System.err.print("Division by zero");
         }
-
+        System.out.print("Do you want continue? No press N / Yes press Y : ");
+        exit = scan.next();
     }
-
 }
+
+
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,15 +1,16 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 //        invoking a static method
-//        Calculator.run();
+//        Calcn.run();
 //        invoking a non static method
         Calculator calculator = new Calculator();
         calculator.run();
-
-
     }
 
 }
